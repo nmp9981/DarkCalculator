f3561786ca44c078aea860d02e58641374d11e5e
tybnasgo <tybnasgo@naver.com>
2026-02-24 04:02:11 +0900

    각 인원별, 종류별 초기 과일 개수 설정

--- Assets/FruitStore/Script/InGame/FruitGameManager.cs
+++ Assets/FruitStore/Script/InGame/FruitGameManager.cs
@@ -8,6 +8,7 @@ public struct FruitImageData
 {
     public string fruitName;
     public Image fruitImage;
+    public int restCount;
 }
 public class FruitGameManager : MonoBehaviour
 { 
@@ -37,6 +38,7 @@ public class FruitGameManager : MonoBehaviour
             if (!fruitImageDic.ContainsKey(data.fruitName))
             {
                 fruitImageDic.Add(data.fruitName, data.fruitImage);
+                fruitTypeCountDic.Add(data.fruitName, 0);
             }
         }
     }
@@ -44,14 +46,17 @@ public class FruitGameManager : MonoBehaviour
     #region µ•¿Ã≈Õ
     private int totalRound =4;
     private int peopleCount;
+    private int fruitTypeCount;
 
     public int maxPeopleCount = 20;
     public int TotalRound { get { return totalRound; } }
     public int PeopleCount { get { return peopleCount; } set { peopleCount = value; } }
+    public int FruitTypeCount { get { return fruitTypeCount; } set { fruitTypeCount = value; } }
 
     public List<string> UserNameList = new List<string>();
     public List<UserInfoClass> UserInfoList  = new List<UserInfoClass>();
     public List<FruitImageData> fruitImageList = new List<FruitImageData>();
     public Dictionary<string, Image> fruitImageDic = new Dictionary<string, Image>();
+    public Dictionary<string, int> fruitTypeCountDic = new Dictionary<string, int>();
     #endregion
 }
--- Assets/FruitStore/Script/UI/FruitSettingEachUser.cs
+++ Assets/FruitStore/Script/UI/FruitSettingEachUser.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class FruitSettingEachUser : MonoBehaviour
@@ -7,11 +8,12 @@ public class FruitSettingEachUser : MonoBehaviour
     [SerializeField]
     Transform scrollTransform;
 
-    public GameObject[] userBlockList = new GameObject[FruitGameManager.Instance.maxPeopleCount];
+    public GameObject[] userBlockList;
 
     void Start()
     {
         //유저 리스트 블록 생성
+        userBlockList = new GameObject[FruitGameManager.Instance.maxPeopleCount];
         for (int i = 0; i < FruitGameManager.Instance.maxPeopleCount; i++)
         {
             userBlockList[i] = Instantiate(userInfoPrefab);
@@ -25,10 +27,38 @@ public class FruitSettingEachUser : MonoBehaviour
     }
 
     /// <summary>
+    /// 초기 과일 개수 설정
+    /// </summary>
+    void InitEachFruitCount()
+    {
+        //총 인원
+        int totalNum = FruitGameManager.Instance.PeopleCount;
+
+        //과일 종류 개수 정하기
+        if (totalNum < 10) FruitGameManager.Instance.FruitTypeCount = (totalNum + 1) / 2;
+        else FruitGameManager.Instance.FruitTypeCount = totalNum / 2;
+
+        int div = (2*totalNum) / FruitGameManager.Instance.FruitTypeCount;//몫
+        int rest = (2 * totalNum) % FruitGameManager.Instance.FruitTypeCount;//나머지
+
+        int idx = 0;
+        var keys = new List<string>(FruitGameManager.Instance.fruitTypeCountDic.Keys);//키를 배열로
+        foreach (var key in keys)
+        {
+            int targetValue = (idx < rest) ? (div + 1) : div;//최대 과일 종류 개수
+            FruitGameManager.Instance.fruitTypeCountDic[key] = targetValue;
+            idx++;
+        }
+    }
+
+    /// <summary>
     /// 유저 정보 등록
     /// </summary>
     void EnrollUserInfo()
     {
+        //초기 과일 개수 설정
+        InitEachFruitCount();
+
         for (int i = 0; i < FruitGameManager.Instance.PeopleCount; i++)
         {
             userBlockList[i].SetActive(true);
