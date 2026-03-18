547883db2334cb17d090bdbc47d481f5585410fd
tybnasgo <tybnasgo@naver.com>
2026-03-18 22:27:40 +0900

    각 유저별 최종 결과 공개시 비공개일 경우도 가격 동시 공개

--- Assets/FruitStore/Script/UI/ShowDetailResult.cs
+++ Assets/FruitStore/Script/UI/ShowDetailResult.cs
@@ -66,12 +66,12 @@ public class ShowDetailResult : MonoBehaviour
                 if (clickUser.sellMoneyRound[curRd, idx].isSecret)
                 {
                     roundResultList[curRd].text +=
-                        $"{clickUser.sellMoneyRound[curRd, idx].name} 비공개\n\n";
+                        $"{clickUser.sellMoneyRound[curRd, idx].name} 비공개\n{clickUser.sellMoneyRound[curRd, idx].money}원\n\n";
                 }
                 else
                 {
                     roundResultList[curRd].text +=
-                        $"{clickUser.sellMoneyRound[curRd, idx].name} {clickUser.sellMoneyRound[curRd, idx].money}\n\n";
+                        $"{clickUser.sellMoneyRound[curRd, idx].name} {clickUser.sellMoneyRound[curRd, idx].money}원\n\n";
                 }
             }
         }
